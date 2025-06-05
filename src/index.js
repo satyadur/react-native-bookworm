5d9e98b79e122474eeaeb4a9dc235fa41089a6bb
satyadur <satyakudupudi8500@gmail.com>
2025-06-05 09:07:12 +0530

    cron added

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import job from "./lib/cron.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+job.start();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -10,7 +10,6 @@ const generateToken = (userId) => {
 
 router.post("/register", async (req, res) => {
   try {
-    console.log(req.body);
     const { email, username, password } = req.body;
     if (!username || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
